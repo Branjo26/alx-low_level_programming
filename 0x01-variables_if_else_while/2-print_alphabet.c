2410737a2d7b664bca8df16ad644de8d94a32d20
Branjo26 <briannjorogemuchiri@gmail.com>
2023-07-16 08:08:24 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+
+int main() 
+{
+    int z;
+
+    for (z = 0; z < 26; z++) {
+        putchar('a' + z);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
